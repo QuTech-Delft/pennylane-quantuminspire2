69c9bb09415690d58a4ff6b3bdf3e8038a895820
Napoleon Cornejo <ncornejo@gmail.com>
2024-10-15 16:10:54 +0200

    [QI2-1096] static checks

--- pennylane_quantuminspire2/qi_device.py
+++ pennylane_quantuminspire2/qi_device.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 from pennylane_qiskit import RemoteDevice
 from qiskit_quantuminspire.qi_backend import QIBackend
 
-class QI2Device(RemoteDevice):
 
-    def __init__(self, backend: QIBackend, **kwargs) -> None:
-        super().__init__(wires=backend.num_qubits, backend=backend,
-                         shots=backend.default_shots, **kwargs)
+class QI2Device(RemoteDevice):  # type: ignore[misc]
+
+    def __init__(self, backend: QIBackend, **kwargs: Any) -> None:
+        super().__init__(wires=backend.num_qubits, backend=backend, shots=backend.default_shots, **kwargs)
