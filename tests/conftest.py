12de8df16bb78222507f734ec3b47357c3c1e3af
napocornejo <ncornejo@geokapti.nl>
2024-10-23 13:23:26 +0200

    [QI2-1096] add method to obtain backends. Update README file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,31 @@
+from typing import Sequence
+from unittest.mock import AsyncMock
+
 import pytest
 from compute_api_client import BackendStatus, BackendType
+from pytest_mock import MockerFixture
+from qiskit_quantuminspire.api.pagination import PageReader
 from qiskit_quantuminspire.qi_backend import QIBackend
 
 
 @pytest.fixture
-def QI2_backend() -> BackendType:
-    """Backend fixture for fields we care about."""
+def page_reader_mock(mocker: MockerFixture) -> AsyncMock:
+    # Simply calling mocker.patch() doesn't work because PageReader is a generic class
+    page_reader_mock = AsyncMock()
+    page_reader_mock.get_all = AsyncMock()
+    page_reader_mock.get_single = AsyncMock()
+    mocker.patch.object(PageReader, "get_all", page_reader_mock.get_all)
+    mocker.patch.object(PageReader, "get_single", page_reader_mock.get_single)
+    return page_reader_mock
+
+
+def create_backend(id: int = 1, name: str = "qi_backend") -> BackendType:
     backend_type = BackendType(
-        name="qi_backend",
+        name=name,
         nqubits=3,
         gateset=["x", "sdag", "prep_y", "measure_z", "h"],
         topology=[[0, 1], [1, 2], [2, 0]],
-        id=1,
+        id=id,
         is_hardware=True,
         image_id="qi_backend",
         features=[],
@@ -24,3 +38,17 @@ def QI2_backend() -> BackendType:
         native_gateset="",
     )
     return QIBackend(backend_type=backend_type)
+
+
+@pytest.fixture
+def QI2_backend() -> BackendType:
+    """Backend fixture for fields we care about."""
+    return create_backend()
+
+
+@pytest.fixture
+def backend_repository(page_reader_mock: AsyncMock) -> Sequence[BackendType]:
+    return [
+        create_backend(),
+        create_backend(10, "qi_backend_10"),
+    ]
--- tests/test_device.py
+++ tests/test_device.py
@@ -1,4 +1,7 @@
+from typing import Sequence
+
 import pennylane as qml
+from compute_api_client import BackendType
 from pytest_mock import MockerFixture
 from qiskit_quantuminspire.qi_backend import QIBackend
 
@@ -16,3 +19,27 @@ def test_device_circuit(mocker: MockerFixture, QI2_backend: QIBackend) -> None:
 
     mocker.patch("qiskit_quantuminspire.qi_backend.QIJob")
     assert quantum_function() is not None
+
+
+def test_device_backends(mocker: MockerFixture, backend_repository: Sequence[BackendType]) -> None:
+    provider_mock = mocker.Mock()
+    provider_mock.backends.return_value = backend_repository
+    QI2Device._qi_provider = provider_mock
+    # Act
+    backends = QI2Device.get_backends()
+
+    # Assert
+    provider_mock.backends.assert_called_once()
+    assert len(backends) == 2
+
+
+def test_device_backend(mocker: MockerFixture, backend_repository: Sequence[BackendType]) -> None:
+    provider_mock = mocker.Mock()
+    provider_mock.get_backend.return_value = backend_repository
+    QI2Device._qi_provider = provider_mock
+    # Act
+    backend = QI2Device.get_backend(name="qi_backend_10")
+
+    # Assert
+    provider_mock.get_backend.assert_called_once()
+    assert backend is not None
