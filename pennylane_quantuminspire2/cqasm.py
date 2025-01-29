3eb61cbba93c699367bf8dca97439b3e1d43e257
Elias KA <elias.el.khaldi@gmail.com>
2025-01-29 13:01:36 +0100

    [QI2-1292] Add **options for function args

--- pennylane_quantuminspire2/cqasm.py
+++ pennylane_quantuminspire2/cqasm.py
@@ -3,10 +3,10 @@ from qiskit import QuantumCircuit
 from qiskit_quantuminspire import cqasm
 
 
-def dumps(q_node: qml.QNode) -> str:
+def dumps(q_node: qml.QNode, **options) -> str:
     """Return the cQASM representation of the quantum function."""
     tape = qml.workflow.construct_tape(q_node)
-    openqasm_code = tape().to_openqasm()
+    openqasm_code = tape(**options).to_openqasm()
 
     quantum_circuit = QuantumCircuit.from_qasm_str(openqasm_code)
     result: str = cqasm.dumps(quantum_circuit)
