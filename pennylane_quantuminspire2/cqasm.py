0d44524363f4797f2980cd62dbfca0fb3317e5a8
Elias KA <elias.el.khaldi@gmail.com>
2025-01-30 16:53:10 +0100

    [QI2-1292] Upgrade to latest version of qiskit-quantuminspire

--- pennylane_quantuminspire2/cqasm.py
+++ pennylane_quantuminspire2/cqasm.py
@@ -5,10 +5,15 @@ from qiskit import QuantumCircuit
 from qiskit_quantuminspire import cqasm
 
 
-def dumps(q_node: qml.QNode, **options: Any) -> str:
+def dumps(q_node: qml.QNode, *args: Any, **kwargs: Any) -> str:
     """Return the cQASM representation of the quantum function."""
-    tape = qml.workflow.construct_tape(q_node)
-    openqasm_code = tape(**options).to_openqasm()
+
+    q_node.construct(args=args, kwargs=kwargs)
+    openqasm_code = q_node.tape.to_openqasm()
+
+    # As of pennylane v0.40.0 Qnode.tape property is no longer supported and instead the following approach is advised:
+    # tape = qml.workflow.construct_tape(q_node)
+    # openqasm_code = tape(*args, **kwargs).to_openqasm()
 
     quantum_circuit = QuantumCircuit.from_qasm_str(openqasm_code)
     result: str = cqasm.dumps(quantum_circuit)
