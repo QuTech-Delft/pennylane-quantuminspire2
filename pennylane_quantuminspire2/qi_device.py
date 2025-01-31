19a05887759367227d9df5a5264cebe1a54bbfb7
Elias KA <elias.el.khaldi@gmail.com>
2025-01-31 16:48:29 +0100

    [QI2-1292] Seperate QI2 backend provider

--- pennylane_quantuminspire2/qi_device.py
+++ pennylane_quantuminspire2/qi_device.py
@@ -1,18 +1,8 @@
-from typing import Any, Optional, Sequence
-
-from pennylane_qiskit import RemoteDevice
+from typing import Any
 from qiskit_quantuminspire.qi_backend import QIBackend
 
 
-class QI2Device(RemoteDevice):  # type: ignore[misc]
+class QI2BackendProvider(RemoteDevice):  # type: ignore[misc]
 
     def __init__(self, backend: QIBackend, **kwargs: Any) -> None:
         super().__init__(wires=backend.num_qubits, backend=backend, **kwargs)
-
-    @classmethod
-    def backends(cls) -> Sequence[QIBackend]:
-        return cls._qi_provider.backends()  # type: ignore[no-any-return]
-
-    @classmethod
-    def get_backend(cls, name: Optional[str] = None, id: Optional[int] = None) -> QIBackend:
-        return cls._qi_provider.get_backend(name, id)
