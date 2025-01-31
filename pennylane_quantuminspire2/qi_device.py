f33878a6f77732463716d385acda410a7838d6d9
Elias KA <elias.el.khaldi@gmail.com>
2025-01-31 16:50:02 +0100

    [QI2-1292] Fix typo

--- pennylane_quantuminspire2/qi_device.py
+++ pennylane_quantuminspire2/qi_device.py
@@ -2,7 +2,7 @@ from typing import Any
 from qiskit_quantuminspire.qi_backend import QIBackend
 
 
-class QI2BackendProvider(RemoteDevice):  # type: ignore[misc]
+class QI2Device(RemoteDevice):  # type: ignore[misc]
 
     def __init__(self, backend: QIBackend, **kwargs: Any) -> None:
         super().__init__(wires=backend.num_qubits, backend=backend, **kwargs)
