92abddfc47a2a4b9cf8ca63be464bf6c388f62e8
Nischal Sehrawat <nischal.sehrawat@tno.nl>
2025-02-03 17:29:18 +0100

    Fixed linting errors for simple.py

--- docs/hybrid/simple.py
+++ docs/hybrid/simple.py
@@ -1,14 +1,16 @@
 from typing import Any, Dict, List
+
 import pennylane as qml
 from pennylane import numpy as np
-from qiskit_quantuminspire.hybrid.hybrid_backend import QIHybridBackend
 from qi2_shared.hybrid.quantum_interface import QuantumInterface
-from pennylane_quantuminspire2 import cqasm
 from qiskit import QuantumCircuit
+from qiskit_quantuminspire.hybrid.hybrid_backend import QIHybridBackend
 from scipy.optimize import minimize
+
 from pennylane_quantuminspire2.qi_device import QI2Device
 
 resultstring = ""
+
 
 def generate_circuit(device: QI2Device) -> qml.QNode:
     # Define a simple Hamiltonian
@@ -20,14 +22,16 @@ def generate_circuit(device: QI2Device) -> qml.QNode:
         qml.RY(params[1], wires=1)
         qml.CNOT(wires=[0, 1])
         return qml.expval(H)
-    
+
     return circuit
+
 
 def convert_to_qiskit(q_node: qml.QNode, *args: Any, **kwargs: Any) -> QuantumCircuit:
     """Return the Qiskit QuantumCircuit representation of the quantum function."""
     q_node.construct(args=args, kwargs=kwargs)
     openqasm_code = q_node.tape.to_openqasm()
     return QuantumCircuit.from_qasm_str(openqasm_code)
+
 
 def execute(qi: QuantumInterface) -> None:
 
@@ -44,16 +48,17 @@ def execute(qi: QuantumInterface) -> None:
         expectation_value = sum([int(state, 2) * count for state, count in counts.items()]) / 1024
         return expectation_value
 
-    optimizer = minimize(cost_function, initial_params, method='COBYLA', options={'maxiter': 100})
-    
+    optimizer = minimize(cost_function, initial_params, method="COBYLA", options={"maxiter": 100})
+
     opt_params = optimizer.x
     final_energy = optimizer.fun
 
     global resultstring
-    resultstring = f"Optimization complete.\n"
+    resultstring = "Optimization complete.\n"
     resultstring += f"Final Energy = {final_energy:.6f}\n"
     resultstring += f"Optimal Parameters = {opt_params}\n"
     print(resultstring)
+
 
 def finalize(list_of_measurements: List[Dict[str, Any]]) -> Dict[str, Any]:
     global resultstring
